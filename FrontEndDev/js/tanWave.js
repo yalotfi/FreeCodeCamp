ed0745ac187cfed2c4bce98764a73c47739b886d
Yaseen Lotfi <yalotfi@outlook.com>
2017-02-21 12:46:07 -0500

    canvas animation

---
+++ FrontEndDev/js/tanWave.js
@@ -0,0 +1,42 @@
+var xspacing = 8;		// Distance between horizintal locations
+var w;					// Width of plot
+var theta = 0.0;		// Starting angle theta
+var amplitude = 25.0;	// Height of wave
+var period = 100.0;		// Pixels before wave repeats
+var dx;					// Increment x by dx
+var yvalues;			// Array that stores height values
+
+function setup() {
+    var navWidth = document.getElementById('midNavSection').offsetWidth;
+    var navHeight = document.getElementById('midNavSection').offsetHeight;
+    console.log(navWidth, navHeight);
+    var canvas = createCanvas(768, 50);
+    canvas.parent('midNavSection');
+    w = width + 8;
+    dx = (TWO_PI / period) * xspacing;
+    yvalues = new Array(floor(w/xspacing));
+    }
+
+function draw() {
+    background('#222');
+    calcWave();
+    renderWave();
+}
+
+function calcWave() {
+	theta += 0.02;
+	
+	var x = theta;
+	for(var i = 0; i < yvalues.length; i++) {
+		yvalues[i] = sin(x) * amplitude;
+		x += dx;
+	}
+}
+
+function renderWave() {
+	noStroke();
+	fill(51, 173, 255);
+	for(var x = 0; x < yvalues.length; x++) {
+		ellipse(x * xspacing, height/2 + yvalues[x], 8, 8);
+	}
+}
