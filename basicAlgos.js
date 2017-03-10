3b30823ce3bd392c208a38deae61c36349efd3b9
Yaseen Lotfi <yalotfi@outlook.com>
2017-03-10 13:56:33 -0500

    more work on basic algorithms

--- basicAlgos.js
+++ basicAlgos.js
@@ -67,3 +67,92 @@ function findLongestWord(sentence) {
   return Math.max.apply(null, lengthArray);
 }
 
+
+// 5) Capitalize First Letters in a sentence:
+function titleCase(str) {
+  // Declare local variables:
+  var strArray = str.toLowerCase().split(" ");
+  var capIndex = [];
+  var output = [];
+
+  // Proper capitalizations:
+  for (var i=0; i < strArray.length; i++) {
+    var length = strArray[i].length;
+    capIndex[i] = strArray[i][0].toUpperCase();
+    output[i] = capIndex[i] + strArray[i].substr(1, length);
+  }
+  return output.join(" ");
+}
+
+
+// 6) Return Largest Values of each element in an Array:
+function largestOfFour(arr) {
+  var output = [];
+
+  for (var i=0; i < arr.length; i++) {
+    var maxElement = Math.max.apply(null, arr[i]);
+    output.push(maxElement);
+  }
+
+  return output;
+}
+
+
+// 7) Check last string against input string:
+//Solution 1 (Robust):
+function confirmEnding01(str, target) {
+  // Look for spaces to check if string is a word or sentence:
+  if (str.match(/\s/gi) === null) {
+    // Store string and target length to compare position, NOT index:
+    var strL = str.length;
+    var tarL = target.length;
+
+    // If the target letters match the ending letters of the word, true.
+    if (str.substr(strL - tarL) === target ) {
+      return true;
+    } else {
+      return false;
+    }
+  } else {
+    // Input is not a word, so split into array of words.
+    var strArray = str.toLowerCase().split(" ");
+    var arrayLen = strArray.length - 1;  // Store array length for Indexing
+
+    // If the final array element equals the target, true.
+    if (strArray[arrayLen] === target) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+}
+
+
+// Solution 2 (Simple):
+function confirmEnding02(str, target) {
+  // Store string and target length to compare position, NOT index:
+  var strL = str.length;
+  var tarL = target.length;
+
+  // If the target letters match the ending letters of the word, true.
+  if (str.substr(strL - tarL) === target ) {
+    return true;
+  } else {
+    return false;
+}
+
+
+// 8) Repeat a given string num, number of times:
+function repeatStringNumTimes(str, num) {
+  if (num > 0) {
+    var output = [];
+    var i = 1;
+    while (i <= num) {
+      output.push(str);
+      i += 1;
+    }
+    return output.join("");
+  } else {
+    return "";
+  }
+}
