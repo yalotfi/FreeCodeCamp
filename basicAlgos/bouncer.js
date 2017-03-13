cd84a70f288988ceea0f4ae27c7ed979d0289ffc
Yaseen Lotfi <yalotfi@outlook.com>
2017-03-13 13:26:18 -0400

    exclude elements of an array by set difference

--- basicAlgos/bouncer.js
+++ basicAlgos/bouncer.js
@@ -2,10 +2,27 @@ function bouncer(arr) {
   // Store values to exclude from output
   var illegal = [false, null, 0, "", undefined, NaN];
 
-  return this.filter(function(illegal) {
-    return arr.indexOf(illegal) < 0;
+  // Perform set difference
+  console.log("Uncleaned Array Length: ", arr.length)
+  var cleanedArray = arr.filter(function(x) {
+    return illegal.indexOf(x) < 0;
   });
+
+  // Set difference not removing NaN:
+  for (var i=0; i < cleanedArray.length+1; i++) {
+    if (isNaN(cleanedArray[i])) {
+      cleanedArray.pop(NaN);
+      console.log("Found a NaN at index: ", i);
+    } else {
+      console.log("No NaN at Index", i);
+    }
+  }
+
+  return cleanedArray;
 }
 
 var illegal = [false, null, 0, "", undefined, NaN];
-console.log([7, "ate", "", false, 9].indexOf(illegal));
+// console.log(isNaN(illegal[illegal.length - 1]));
+
+
+console.log(bouncer([1, 3, NaN, 6, false, 10]));
