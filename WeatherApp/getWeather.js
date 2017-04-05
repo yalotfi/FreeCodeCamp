fc8950c424a625209897daa686fc75ab7f682b1c
Yaseen Lotfi <yalotfi@outlook.com>
2017-04-05 10:34:10 -0400

    call coordinate info from browser

--- WeatherApp/getWeather.js
+++ WeatherApp/getWeather.js
@@ -1,24 +1,33 @@
-'http://api.openweathermap.org/data/2.5/weather?lat=36&lon=-77&units=metric&APPID=31f8be28b38ac24b868953a57805d5d6&callback=JSON_CALLBACK'
+//'http://api.openweathermap.org/data/2.5/weather?lat=36&lon=-77&units=metric&APPID=31f8be28b38ac24b868953a57805d5d6&callback=JSON_CALLBACK'
 
-var call = 'http://api.openweathermap.org/data/2.5/weather?';
-var coords = 'lat=' + '&lon=';
+var api = 'http://api.openweathermap.org/data/2.5/weather?';
+var latCoords = 'lat=';
+var lonCoords = '&lon=';
 var units = '&units=metric';
 var key = '31f8be28b38ac24b868953a57805d5d6&callback';
 var end = '&APPID=' + key + '=JSON_CALLBACK';
-var api = call + coords + units + end;
 
 function getLocation() {
-    var geo = navigator.geolocation
-    if (geo) {
-        geo.getCurrentPosition(showPosition);
-    } else {
-        console.log('Browser Not Supported');
-    }
-}
-function showPosition(position) {
-    var pos = position.coords
-    console.log(pos.latitude);
-    console.log(pos.longitude);
+  var geo = navigator.geolocation
+  var out = document.getElementById("output");
+
+  function showPosition(position) {
+      var latitude = position.coords.latitude;
+      var longitude = position.coords.longitude;
+      out.innerHTML = latitude + ' ' + longitude;
+  }
+
+  function errorHandle() {
+    out.innerHTML = '<p>Unable to Determine Location</p>';
+  }
+
+  out.innerHTML = '<p>Locating...</p>';
+
+  if (!geo) {
+      out.innerHTML = '<p>Browser Not Supported</p>';
+  } else {
+      geo.getCurrentPosition(showPosition, errorHandle);
+  }
 }
 
 getLocation();
