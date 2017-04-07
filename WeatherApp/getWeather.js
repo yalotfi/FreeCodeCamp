f71da77e2e2d246e2f7247fcbbdb791f90261aca
Yaseen Lotfi <yalotfi@outlook.com>
2017-04-07 16:09:36 -0400

    call API, pull data, and populate page

--- WeatherApp/getWeather.js
+++ WeatherApp/getWeather.js
@@ -1,33 +1,43 @@
+var api;
+var link = 'http://api.openweathermap.org/data/2.5/weather?';
+var latCoords = 'lat=';
+var lonCoords = '&lon=';
+var units = '&units=metric';
+var key = '31f8be28b38ac24b868953a57805d5d6&callback';
+var call = '&APPID=' + key;
+
 function getData() {
-  var api;
-  var link = 'http://api.openweathermap.org/data/2.5/weather?';
-  var latCoords = 'lat=';
-  var lonCoords = '&lon=';
-  var units = '&units=metric';
-  var key = '31f8be28b38ac24b868953a57805d5d6&callback';
-  var end = '&APPID=' + key + '=JSON_CALLBACK';
-  var out = document.getElementById("output");
-  $('#output').text('Loading...');
+  /************************
+  Check if geolocation is possible then pull API data
+  and populate webpage.
+  *************************/
   if (!navigator.geolocation) {
-    $('#output').text('Browser Not Supported');
+    $('#Status').append('<p>Browser Not Supported</p>');
   } else {
     navigator.geolocation.getCurrentPosition(function(position) {
-      var longitude = position.coords.longitude;
-      var latitude = position.coords.latitude;
-      latCoords += latitude;
-      lonCoords += longitude;
-      api = link+latCoords+lonCoords+units+key+end;
+      var lat = position.coords.latitude;
+      var lon = position.coords.longitude;
+      latCoords += lat;
+      lonCoords += lon;
+      api = link + latCoords + lonCoords + call + units;
       $.getJSON(api, function(data) {
-        var lon = data.coord.lon;
-        var lat = data.coord.lat;
-        console.log([lon, lat]);
+        var weather = data['weather'][0];
+        $('#Temp').text(data.main.temp);
+        $('#City').text(data.name + ', ' + data.sys.country);
+        $('#Conditions').text(weather.main);
+        console.log(data)
       });
     }, function() {
-      $('#output').text('Unable to Determine Location');
+      $('#Status').append('<p>Unable to Determine Location</p>');
     });
   }
 }
 
 $(document).ready(function() {
+  /************************
+  City element will always be populated and a new
+  element will be created if an error occurs.
+  *************************/
+  $('#City').text('Locating...');
   getData();
 });
