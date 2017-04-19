d0a0ac863702661df9d335fb194aa4dac30d00e2
Yaseen Lotfi <yalotfi@outlook.com>
2017-04-19 09:47:49 -0400

    wiki api string

--- WikiViewer/getData.js
+++ WikiViewer/getData.js
@@ -1,9 +1,14 @@
-$.ajax({
-  url: "https://en.wikipedia.org/w/api.php?action=query&titles=Main%20Page&prop=revisions&rvprop=content&format=json",
-  dataType: 'json',
-  type: 'POST',
-  headers: { 'Api-User-Agent': 'Example/1.0' },
-  success: function(data) {
-   console.log(data)
-  }
+var wikiLink = 'https://en.wikipedia.org/wiki/';
+
+$(document).ready(function(){
+  $.ajax({
+    type: "GET",
+    url: "http://en.wikipedia.org/w/api.php?action=parse&format=json&prop=text&section=0&page=Jimi_Hendrix&callback=?",
+    contentType: "application/json; charset=utf-8",
+    async: false,
+    dataType: "json",
+    success: function (data, textStatus, jqXHR) {
+      console.log(data);
+    }
+  });
 });
